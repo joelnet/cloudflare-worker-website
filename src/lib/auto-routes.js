1fcd52001bd8e3ca9a4810b138979af6a50f5a2a
Joel Thoms <jthoms@gmail.com>
2021-01-24 16:35:04 -0800

    refactor: 💡 simplify auto-routes.js

--- src/lib/auto-routes.js
+++ src/lib/auto-routes.js
@@ -5,13 +5,13 @@ const isModule = module => {
 
 const hasRoute = module => {
 	if (!module || !module.exports) return false
-	return module.exports.route
+	return !!module.exports.route
 }
 
 export const getRoutes = () =>
-	Object.entries(require.cache)
-		.filter(([, module]) => isModule(module) && hasRoute(module))
-		.map(([, module]) => ({
+	Object.values(require.cache)
+		.filter(module => isModule(module) && hasRoute(module))
+		.map(module => ({
 			route: module.exports.route,
 			method: module.exports.method || 'get',
 			module: module.exports.default,
